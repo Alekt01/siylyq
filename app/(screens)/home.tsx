095f8b27923ddd1fe9a74abb0617b92fab86b38b
absolutetsonga <abstsonga@gmail.com>
2023-08-02 14:01:30 +0600

    modified: git last commit

--- app/(screens)/home.tsx
+++ app/(screens)/home.tsx
@@ -4,13 +4,104 @@ import {
   Image,
   SafeAreaView,
   ScrollView,
+  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 
-import { styles } from '../(auth)/login';
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerColor: {
+    flex: 1,
+    backgroundColor: '#F8FAFF',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  inputView: {
+    borderRadius: 5,
+    width: '90%',
+    height: 45,
+    marginBottom: 20,
+    borderColor: '#000',
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+
+  textInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+
+  primaryButton: {
+    width: '90%',
+    borderRadius: 5,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
+    backgroundColor: '#000',
+    color: '#ffffff',
+  },
+
+  primaryButtonText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+
+  titleText: {
+    color: '#000',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+
+  logo: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 40,
+  },
+
+  footer: {
+    color: '#000',
+    marginTop: 20,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+
+  secondaryButton: {
+    marginTop: 400,
+    borderRadius: 5,
+    padding: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#6ee7b7',
+  },
+
+  secondaryButtonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  oauthView: {
+    width: '90%',
+    marginBottom: 20,
+    marginTop: 100,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+});
 
 const Home = () => {
   const [searchValue, setSearchValue] = useState('');
@@ -179,10 +270,10 @@ const Home = () => {
   };
 
   return (
-    <SafeAreaView style={styles.containerPink}>
+    <SafeAreaView style={styles.containerColor}>
       <View className="px-5 py-6 pb-60">
         <Text className="mb-10 mt-14 text-5xl font-semibold text-black">
-          Syulyq
+          Siylyq
         </Text>
 
         <View className="mb-4 flex w-full flex-row-reverse items-center justify-center rounded-3xl bg-gray-50">
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,14 @@
-import { ClerkProvider } from '@clerk/clerk-expo';
 import { Stack } from 'expo-router';
 
-import { AuthProvider } from '@/contexts/protected-provider';
-
-import { tokenCache } from '../cache';
-
-const CLERK_PUBLISHABLE_KEY =
-  'pk_test_ZmxlZXQtZGFuZS03MS5jbGVyay5hY2NvdW50cy5kZXYk';
 export default function Layout() {
   return (
-    <ClerkProvider
-      publishableKey={CLERK_PUBLISHABLE_KEY}
-      tokenCache={tokenCache}
-    >
-      <AuthProvider>
-        <Stack>
-          <Stack.Screen
-            name="(screens)"
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="(auth)/login"
-            options={{
-              headerShown: false,
-            }}
-          />
-        </Stack>
-      </AuthProvider>
-    </ClerkProvider>
+    <Stack>
+      <Stack.Screen
+        name="(screens)"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
   );
 }
